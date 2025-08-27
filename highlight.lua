fdd7b1411aa1686217a8485f271f96926b41e0b9
m52ffsd211 <maxyatskiv1234@gmail.com>
2025-08-27 19:13:55 +0300

    Update highlight.lua

--- highlight.lua
+++ highlight.lua
@@ -1,3 +1,19 @@
+local me = game.Players.LocalPlayer
+local gay = nil
+
+for i,v in pairs(workspace:GetDescendants()) do
+    if v.Name == "TextLabel" then
+        if v.Text:match(me.DisplayName, 1, true) then
+            gay = v
+        end
+    end
+end
+
+
+local bas = gay.Parent.Parent.Parent.Parent
+
+
+
 local function parseNumber(str)
     str = str:lower():gsub(",", ""):gsub("%s","")
     str = str:gsub("%$", "") 
@@ -23,21 +39,23 @@ end
 local maxValue = nil
 local maxObj = nil
 
-
-for i,v in pairs(game:GetDescendants()) do
+for i,v in pairs(workspace.Plots:GetDescendants()) do
     if v.Name == "Generation" then
-        print(v.Text)
         local num = parseNumber(v.Text)
         if num then
+            if v.Parent.Parent.Parent.Parent.Parent.Parent.Parent.Name ~= bas.Name then
             if not maxValue or num > maxValue then
                 maxValue = num
                 maxObj = v
+            end
             end
         end
     end
 end
 
+
 local part = maxObj.Parent.Parent.Parent.Parent.Decorations.Part
+
 
 
 local billboard = Instance.new("BillboardGui")
@@ -57,6 +75,8 @@ then
 })
 end
 
+
+
 local function formatNumber(num)
     if num >= 1e6 then
         return string.format("%.1fM/s", num/1e6)
@@ -70,8 +90,9 @@ end
 local formatted = formatNumber(maxValue)
 
 
+
 billboard.Adornee = part            
-billboard.Size = UDim2.new(0, 200, 0, 50) 
+billboard.Size = UDim2.new(0, 100, 0, 20) 
 billboard.StudsOffset = Vector3.new(0, 3, 0) 
 billboard.AlwaysOnTop = true       
 billboard.Parent = workspace       
@@ -83,8 +104,56 @@ textLabel.Size = UDim2.new(1, 0, 1, 0)
 textLabel.BackgroundTransparency = 1
 textLabel.Text = tostring(formatted)
 textLabel.TextScaled = true
-textLabel.TextColor3 = Color3.new(100/255, 0, 255/255)  
+textLabel.TextColor3 = Color3.new(255/255, 0, 0)  
 textLabel.TextStrokeTransparency = 0                       
 textLabel.TextStrokeColor3 = Color3.new(0,0,0)            
 textLabel.Font = Enum.Font.ArialBold
+
+local dn = part.Parent.Parent.Spawn.Attachment.AnimalOverhead.DisplayName
+
+
+
+
+
+local billboard2 = Instance.new("BillboardGui")
+billboard2.Adornee = part            
+billboard2.Size = UDim2.new(0, 100, 0, 20) 
+billboard2.StudsOffset = Vector3.new(0, 8, 0) 
+billboard2.AlwaysOnTop = true       
+billboard2.Parent = workspace 
+
+local textLabel = Instance.new("TextLabel")
+textLabel.Parent = billboard2
+textLabel.Size = UDim2.new(1, 0, 1, 0)
+textLabel.BackgroundTransparency = 1
+textLabel.Text = tostring(dn.Text)
+textLabel.TextScaled = true
+textLabel.TextColor3 = Color3.new(255/255, 0, 0)   
+textLabel.TextStrokeTransparency = 0                       
+textLabel.TextStrokeColor3 = Color3.new(0,0,0)            
+textLabel.Font = Enum.Font.ArialBold
+
+local kal = maxObj.Parent.Parent.Parent.Parent
+local ooo = kal.Decorations.Part
+
+
+
+if maxValue
+then
+    game.StarterGui:SetCore("SendNotification", {
+    Title = "Max value:";
+    Text = tostring(formatted);
+    Duration = 3;       
+})
+end
+
+local partik = Instance.new("BoxHandleAdornment")
+partik.Parent = workspace
+partik.Size = Vector3.new(6, 1, 6)
+partik.Transparency = 0.5
+partik.ZIndex = 0
+partik.AlwaysOnTop = true 
+partik.Adornee = ooo
+partik.Color3 = Color3.new(1,0,0)
+
 
